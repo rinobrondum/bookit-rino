59423bcd10dcc58dd9d56b198bffd3cd01ec9b9a
rinobrondum <rinobroendum@gmail.com>
2024-11-14 11:41:32 +0100

    header  component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 import "@/assets/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />  
         <main className="mx-auto max-w-7xl p-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
