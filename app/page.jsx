445e1c2d13e7df7d0f28626e5f159f5db02ac4ed
rinobrondum <rinobroendum@gmail.com>
2024-11-17 18:49:27 +0100

    homepage rooms (json data) opdated

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,10 @@ export default function Home() {
   return (
     <>
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3 key={room.id}>{room.name}</h3>)
+        rooms.map((room) => <h3>{room.name}</h3>)
       ) : (
         <p>No rooms available at the moment</p>
-
       )}
-      
     </>
   );
 }
