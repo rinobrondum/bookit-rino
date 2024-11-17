6fa7711c8774eaed29204e9324d996d2e56f45ee
rinobrondum <rinobroendum@gmail.com>
2024-11-17 19:57:39 +0100

    roomcard component

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
+import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <>
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3>{room.name}</h3>)
+        rooms.map((room) => <RoomCard room={room} />)
       ) : (
         <p>No rooms available at the moment</p>
       )}
