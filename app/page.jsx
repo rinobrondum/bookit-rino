a609ca23feaa3566c8d01867b9a09af36f1051e4
rinobrondum <rinobroendum@gmail.com>
2024-11-19 10:52:00 +0100

    heading component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <>
+    <Heading title="Available Rooms" />	
       {rooms.length > 0 ? (
         rooms.map((room) => <RoomCard room={room} />)
       ) : (
