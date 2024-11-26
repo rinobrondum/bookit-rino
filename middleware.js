e655daca02d67531542561956f3e43505c161e8b
rinobrondum <rinobroendum@gmail.com>
2024-11-26 15:45:43 +0100

    protecting routes

--- middleware.js
+++ middleware.js
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const { pathname } = request.nextUrl;
+  const isAuthenticated = true;
 
-  console.log(`Request Page: ${pathname}`);
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/login"],
+  matcher: ["/bookings"],
 };
