0302c116da40f1218392d3d56fda6354d68e82f0
rinobrondum <rinobroendum@gmail.com>
2024-12-10 12:27:13 +0100

    storage buckets & image upload

--- app/actions/createRoom.js
+++ app/actions/createRoom.js
@@ -3,10 +3,11 @@ import { createAdminClient } from "@/config/appwrite";
 import checkAuth from "./checkAuth";
 import { ID } from "node-appwrite";
 import { revalidatePath } from "next/cache";
+import { Odor_Mean_Chey } from "next/font/google";
 
 async function createRoom(previousState, formData) {
   //Get databases instance
-  const { databases } = await createAdminClient();
+  const { databases, storage } = await createAdminClient();
 
   try {
     const { user } = await checkAuth();
@@ -16,6 +17,26 @@ async function createRoom(previousState, formData) {
         error: "You must be looged in to create a room",
       };
     }
+    // Uploading image
+    let imageID;
+
+    const image = formData.get("image");
+
+    if (image && image.size > 0 && image.name !== "underfined") {
+      try {
+        // Upload
+        const response = await storage.createFile("rooms", ID.unique(), image);
+        imageID = response.$id;
+      } catch (error) {
+        console.log("Error uploading image", error);
+        return {
+          error: "Error uploading image",
+        };
+      }
+    } else {
+      console.log("No image file provided or file is invalid");
+    }
+
     // Create room
     const newRoom = await databases.createDocument(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
@@ -32,6 +53,7 @@ async function createRoom(previousState, formData) {
         availability: formData.get("availability"),
         price_per_hour: formData.get("price_per_hour"),
         amenities: formData.get("amenities"),
+        image: imageID,
       }
     );
 
