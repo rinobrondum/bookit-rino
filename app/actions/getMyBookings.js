f14629a4848bbd0bc4af7e095ae0e6f590e71fc6
rinobrondum <rinobroendum@gmail.com>
2024-12-13 12:48:42 +0100

    bookedroomcard component

--- app/actions/getMyBookings.js
+++ app/actions/getMyBookings.js
@@ -1,26 +1,26 @@
-"use server";
+'use server';
 
-import { createSessionClient } from "@/config/appwrite";
-import { cookies } from "next/headers";
-import { Query } from "node-appwrite";
-import { redirect } from "next/navigation";
-import checkAuth from "./checkAuth";
+import { createSessionClient } from '@/config/appwrite';
+import { cookies } from 'next/headers';
+import { Query } from 'node-appwrite';
+import { redirect } from 'next/navigation';
+import checkAuth from './checkAuth';
 
 async function getMyBookings() {
-  const sessionCookie = cookies().get("appwrite-session");
+  const sessionCookie = cookies().get('appwrite-session');
   if (!sessionCookie) {
-    redirect("/login");
+    redirect('/login');
   }
 
   try {
     const { databases } = await createSessionClient(sessionCookie.value);
 
-    // Get user´s ID
+    // Get user's ID
     const { user } = await checkAuth();
 
     if (!user) {
       return {
-        error: "You must be logged in to view bookings",
+        error: 'You must be logged in to view bookings',
       };
     }
 
@@ -28,14 +28,14 @@ async function getMyBookings() {
     const { documents: bookings } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
       process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_BOOKINGS,
-      [Query.equal("user_id", user.id)]
+      [Query.equal('user_id', user.id)]
     );
 
     return bookings;
   } catch (error) {
-    console.log("Failed to get user bookings", error);
+    console.log('Failed to get user bookings', error);
     return {
-      error: "Failed to get bookings",
+      error: 'Failed to get bookings',
     };
   }
 }
